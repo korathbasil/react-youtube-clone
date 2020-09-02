03ef3532d1a44ffd132c5cee6344c4a82921ef90
korathbasil <korathbasil@live.com>
2020-09-02 23:34:23 +0530

    working on VideoCard

---
+++ src/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./VideoCard.css";
+
+function VideoCard() {
+  return (
+    <div className="videoCard">
+      <img src="" alt="" className="videoCard__Thumbnail" />
+      <div className="videoCard"></div>
+    </div>
+  );
+}
+
+export default VideoCard;
